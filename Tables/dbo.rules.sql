71899857ded1813e7107bc386dcb40728ea6be90
Neil Vingoe <nvingoe@fdbhealth.com>
2014-03-30 23:16:45 +0100

    A new SQL db
    a rules table and hello world

---
+++ Tables/dbo.rules.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[rules]
+(
+[id] [int] NULL,
+[RuleCriteria] [varchar] (100) COLLATE Latin1_General_CI_AS NULL,
+[RuleName] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
