11e9d239868524626bb19e39b20c79bf5713b91e
unknown <vingoene01@HBMEXE-VINGOEN4.CompanyNet.org>
2015-03-30 17:27:16 +0100

    heres a commit

--- Tables/dbo.rules.sql
+++ Tables/dbo.rules.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[rules]
 [id] [int] NULL,
 [RuleCriteria] [varchar] (100) COLLATE Latin1_General_CI_AS NULL,
 [RuleName] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
-[RuleType] [INT] COLLATE Latin1_General_CI_AS NULL
+[RuleType] [INT] COLLATE Latin1_General_CI_AS NULL,
+[RuleStatus] [BIT] COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
